2542ede218badfd89d780d328c5d7c3ae92c30a7
Nina Dupuis <ninadupuis@yahoo.fr>
2024-05-13 10:15:23 +0200

    Modification Slideshow affichage boutons et compteur.

--- kasa/src/components/Slideshow/slideshow.jsx
+++ kasa/src/components/Slideshow/slideshow.jsx
@@ -14,14 +14,17 @@ function Slideshow({ title, pictures }) {
     return (
         <section className='slideshow__container'>
             <div className='slideshow'>
-                <button className={length > 1 ? 'slideshow__btn slideshow__btn--back' : 'slideshow__btn--hidden'} >
-                    <img src={arrowBack} className='slideshow__arrow slideshow__arrow--back' alt="flèche de retour à la photo précédente" onClick={() => setCurrent(current - 1 >= 0 ? current - 1 : length - 1)} />
-                </button>
+                {length > 1 ?
+                    <button className='slideshow__btn slideshow__btn--back'>
+                        <img src={arrowBack} className='slideshow__arrow slideshow__arrow--back' alt="flèche de passage à la photo précédente" onClick={() => setCurrent(current - 1 >= 0 ? current - 1 : length - 1)} />
+                    </button> : null}
                 <img src={pictures[current]} className='slideshow__img' alt={`${title}-${current + 1}`} />
-                <span className='slideshow__index'>{current + 1}/{length}</span>
-                <button className={length > 1 ? 'slideshow__btn slideshow__btn--forward' : 'slideshow__btn--hidden'} >
-                    <img src={arrowForward} className='slideshow__arrow slideshow__arrow--forward' alt="flèche de passage à la photo suivante" onClick={() => setCurrent(current + 1 <= length - 1 ? (current + 1) : 0)} />
-                </button>
+                {length > 1 ?
+                    <span className='slideshow__index'>{current + 1}/{length}</span> : null}
+                {length > 1 ?
+                    <button className='slideshow__btn slideshow__btn--forward'>
+                        <img src={arrowForward} className='slideshow__arrow slideshow__arrow--forward' alt="flèche de passage à la photo suivante" onClick={() => setCurrent(current + 1 <= length - 1 ? (current + 1) : 0)} />
+                    </button> : null}
             </div>
         </section >
     )
