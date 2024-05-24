d9c89deacecf04797c1e2e45649b7c6dba972ea7
Nina Dupuis <ninadupuis@yahoo.fr>
2024-05-24 15:35:20 +0200

    Suppression span dans Error.jsx

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -7,7 +7,6 @@ function Error() {
         <div className='error'>
             <h1 className='error__title'>404</h1>
             <p className='error__paragraph'>Oups! La page que vous demandez n'existe pas.</p>
-            <span></span>
             <Link className='error__link' to="/">Retournez sur la page d'accueil</Link>
         </div>
     )
